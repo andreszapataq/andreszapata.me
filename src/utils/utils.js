c5dd2ec5a4c778d3a38cd12fdc84537da35eb2e5
andreszapataq <andreszapataq@gmail.com>
2023-11-13 17:20:13 -0500

    add formspree to the form

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
 const yearElement = document.getElementById('year');
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear.toString();
+
+window.onbeforeunload = () => {
+    for(const form of document.getElementsByTagName('form')) {
+      form.reset();
+    }
+}
