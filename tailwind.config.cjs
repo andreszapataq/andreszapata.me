6d9efdcbbcdeabb021ca47b8c4869966438b83e4
andreszapataq <andreszapataq@gmail.com>
2024-01-21 15:09:03 -0500

    first commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['Urbanist Variable', ...defaultTheme.fontFamily.sans],
-			},
-			colors: {
-				'bleu-de-france': '#118CEF',
-				'navy-blue': '#0C056D'
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Urbanist Variable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "bleu-de-france": "#118CEF",
+        "navy-blue": "#0C056D",
+      },
+    },
+  },
+  plugins: [],
+};
